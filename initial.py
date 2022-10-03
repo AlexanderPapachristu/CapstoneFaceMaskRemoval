b908c16a4f6a879d55e3a347a7a4224f84a8d761
ZacCarman <zachary.carman@ontariotechu.net>
2022-10-03 11:16:57 -0400

    initial file

---
+++ initial.py
@@ -0,0 +1,13 @@
+import cv2
+import os
+
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder,filename))
+        if img is not None:
+            images.append(img)
+    return images
+
+
+def main():
